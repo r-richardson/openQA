5aa1150c4da20c95b81846caeb63a1ea8045b589
r-richardson <robert.richardson@suse.com>
2026-07-27 17:27:48 +0200

    Optimize comments query in referenced_bugs
    When executing limit_bugs, roughly 519,000+ comment record objects are
    being instantiated, locking the OSD database for ~20s.
    
    Optimize the query by selecting only the 'text' column and returning
    raw Perl scalar strings via get_column('text')->all instead of row
    objects, resulting in near instant query execution.
    
    Related Progress Issue: https://progress.opensuse.org/issues/203295

--- lib/OpenQA/Schema/ResultSet/Comments.pm
+++ lib/OpenQA/Schema/ResultSet/Comments.pm
@@ -76,9 +76,8 @@ Return a hashref of all bugs referenced by job comments.
 =cut
 
 sub referenced_bugs ($self) {
-
-    my $comments = $self->search({-not => {job_id => undef}});
-    my %bugrefs = map { $_ => 1 } map { @{find_bugrefs($_->text)} } $comments->all;
+    my $comments = $self->search({-not => {job_id => undef}}, {select => ["text"]});
+    my %bugrefs = map { $_ => 1 } map { @{find_bugrefs($_)} } $comments->get_column("text")->all;
     return \%bugrefs;
 }
 
